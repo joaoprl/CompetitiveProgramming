dae3d401026e8eff137e33a372ebe68dd8da79bf
jpedrorl <jpedrorl@gmail.com>
2016-11-27 18:21:17 -0200

    Update contest 738 of codeforces

--- codeforces/735/B.cpp
+++ codeforces/735/B.cpp
@@ -1,6 +1,7 @@
 // B. Urbanization
 
-#include <cstdio>
+#include <iostream>
+#include <iomanip>
 #include <algorithm>
 
 using namespace std;
@@ -14,27 +15,29 @@ void sortInts(int &n1, int &n2){
 
 int main(){
   int n, n1, n2;
-  scanf("%d %d %d", &n, &n1, &n2);
+  cin >> n >> n1 >> n2;
+
   int *arr = new int[n];
   for(int i = 0; i < n; i++)
-    scanf("%d", &(arr[i]));
+    cin >> arr[i];
 
   sort(arr, arr + n);
 
   sortInts(n1, n2);
-  long long t1 = 0, t2 = 0;
+
+  long long t1 = 0, t2 = 0, total = 0;
+
   for(int i = n - 1; i >= n - n1; i--)
     t1 += arr[i];
 
   for(int i = n - n1 - 1; i >= n - n1 - n2; i--)
     t2 += arr[i];
 
+  total = t1 * n2 + t2 * n1;
 
   double sMean = 0;
-
-  sMean = t1 / (double)n1 + t2 / (double)n2;
-
-  printf("%.8lf\n", sMean);
+  sMean = total / ((double)n1 * n2);
+  cout << fixed << setprecision(8) << sMean << endl;
 
   return 0;
 }
